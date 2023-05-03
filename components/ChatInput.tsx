82b32fa206efe19f56e31e13bff28a7429a0a780
gamingggwp <gamingggwp47@gmail.com>
2023-05-03 17:31:57 +0800

    modified:   components/ChatInput.tsx 	modified:   lib/queryApi.ts

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -44,7 +44,7 @@ function ChatInput({ chatId }: Props) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                prompt: input, chatId, model, session,
+                prompt: input + ' you', chatId, model, session,
             }),
         }).then(() => {
             //Toast notification to say successful!
--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -5,11 +5,11 @@ const query = async (prompt: string, chatId: string, model: string) => {
         .createCompletion({
             model,
             prompt,
-            temperature: 0.9,
+            temperature: 0.3,
             top_p: 1,
             max_tokens: 1000,
-            frequency_penalty: 0,
-            presence_penalty: 0,
+            frequency_penalty: 0.8,
+            presence_penalty: 0.8,
         })
         .then(res => res.data.choices[0].text)
         .catch(
